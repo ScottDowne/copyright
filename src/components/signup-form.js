89a828c924019da39ed99be6c9e211f8cdc94014
ScottDowne <scott.downe@gmail.com>
2017-03-15 12:22:16 -0400

    Experimental sticky form

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -93,6 +93,7 @@ var Signup = React.createClass({
       <div className="signup-form-container">
         <div id="get-involved" className="nav-anchor nav-offset"></div>
         <div className="signup-form">
+          <div className="input-wrapper">
           <h3>
             {this.context.intl.formatMessage({id: 'take_action_headline2'})}
           </h3>
@@ -108,61 +109,62 @@ var Signup = React.createClass({
               </div>
             </StickyContainer>
           </div>
-          <input autoComplete="off" type='text' value={this.props.firstName} onChange={this.firstNameChange} placeholder={this.context.intl.formatMessage({id: 'first_name'})}/>
-          <input autoComplete="off" type='text' value={this.props.lastName} onChange={this.lastNameChange} placeholder={this.context.intl.formatMessage({id: 'last_name'})}/>
-          <input autoComplete="off" ref={(input) => { this.emailInput = input; }} type='email' className={emailClassName} value={this.props.email} onChange={this.emailChange} required placeholder={this.context.intl.formatMessage({id: 'email'})}/>
-          <select autoComplete="off" required value={this.props.country} onChange={this.countryChange}>
-            <option value="">{this.context.intl.formatMessage({id: 'country'})}</option>
-            <option value="AT">Austria</option>
-            <option value="BE">Belgium</option>
-            <option value="BG">Bulgaria</option>
-            <option value="HR">Croatia</option>
-            <option value="CY">Cyprus</option>
-            <option value="CZ">Czech Republic</option>
-            <option value="DK">Denmark</option>
-            <option value="EE">Estonia</option>
-            <option value="FI">Finland</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
-            <option value="GR">Greece</option>
-            <option value="HU">Hungary</option>
-            <option value="IE">Ireland</option>
-            <option value="IT">Italy</option>
-            <option value="LV">Latvia</option>
-            <option value="LT">Lithuania</option>
-            <option value="LU">Luxembourg</option>
-            <option value="MT">Malta</option>
-            <option value="NL">Netherlands</option>
-            <option value="PL">Poland</option>
-            <option value="PT">Portugal</option>
-            <option value="RO">Romania</option>
-            <option value="SK">Slovakia</option>
-            <option value="SI">Slovenia</option>
-            <option value="ES">Spain</option>
-            <option value="SE">Sweden</option>
-            <option value="GB">United Kingdom</option>
-            <option value="other" data-other="">Other</option>
-          </select>
-          <p className="error-message">{this.props.emailError}</p>
-          <p className="error-message">{this.state.signupError}</p>
-          <label>
-            <input className="checkbox" autoComplete="off" onChange={this.signupCheckboxChange} value={this.props.signupCheckbox} type="checkbox"></input>
-            {this.context.intl.formatMessage({id: 'signup_checkbox'})}
-          </label>
-          <p className="privacy-error error-message">{this.props.signupCheckboxError}</p>
-          <label>
-            <input className="checkbox" autoComplete="off" onChange={this.privacyCheckboxChange} value={this.props.privacyCheckbox} type="checkbox"></input>
-            <FormattedMessage
-              id='sign_up_notice'
-              values={{
-                linkPrivacyNotice: (<a href="https://www.mozilla.org/privacy/websites/">{this.context.intl.formatMessage({id: 'link_pn'})}</a>)
-              }}
-            />
-          </label>
-          <p className="privacy-error error-message">{this.props.privacyCheckboxError}</p>
-          <button onClick={this.onSubmit} className={buttonClassName}>
-            {buttonText}
-          </button>
+            <input autoComplete="off" type='text' value={this.props.firstName} onChange={this.firstNameChange} placeholder={this.context.intl.formatMessage({id: 'first_name'})}/>
+            <input autoComplete="off" type='text' value={this.props.lastName} onChange={this.lastNameChange} placeholder={this.context.intl.formatMessage({id: 'last_name'})}/>
+            <input autoComplete="off" ref={(input) => { this.emailInput = input; }} type='email' className={emailClassName} value={this.props.email} onChange={this.emailChange} required placeholder={this.context.intl.formatMessage({id: 'email'})}/>
+            <select autoComplete="off" required value={this.props.country} onChange={this.countryChange}>
+              <option value="">{this.context.intl.formatMessage({id: 'country'})}</option>
+              <option value="AT">Austria</option>
+              <option value="BE">Belgium</option>
+              <option value="BG">Bulgaria</option>
+              <option value="HR">Croatia</option>
+              <option value="CY">Cyprus</option>
+              <option value="CZ">Czech Republic</option>
+              <option value="DK">Denmark</option>
+              <option value="EE">Estonia</option>
+              <option value="FI">Finland</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+              <option value="GR">Greece</option>
+              <option value="HU">Hungary</option>
+              <option value="IE">Ireland</option>
+              <option value="IT">Italy</option>
+              <option value="LV">Latvia</option>
+              <option value="LT">Lithuania</option>
+              <option value="LU">Luxembourg</option>
+              <option value="MT">Malta</option>
+              <option value="NL">Netherlands</option>
+              <option value="PL">Poland</option>
+              <option value="PT">Portugal</option>
+              <option value="RO">Romania</option>
+              <option value="SK">Slovakia</option>
+              <option value="SI">Slovenia</option>
+              <option value="ES">Spain</option>
+              <option value="SE">Sweden</option>
+              <option value="GB">United Kingdom</option>
+              <option value="other" data-other="">Other</option>
+            </select>
+            <p className="error-message">{this.props.emailError}</p>
+            <p className="error-message">{this.state.signupError}</p>
+            <label>
+              <input className="checkbox" autoComplete="off" onChange={this.signupCheckboxChange} value={this.props.signupCheckbox} type="checkbox"></input>
+              {this.context.intl.formatMessage({id: 'signup_checkbox'})}
+            </label>
+            <p className="privacy-error error-message">{this.props.signupCheckboxError}</p>
+            <label>
+              <input className="checkbox" autoComplete="off" onChange={this.privacyCheckboxChange} value={this.props.privacyCheckbox} type="checkbox"></input>
+              <FormattedMessage
+                id='sign_up_notice'
+                values={{
+                  linkPrivacyNotice: (<a href="https://www.mozilla.org/privacy/websites/">{this.context.intl.formatMessage({id: 'link_pn'})}</a>)
+                }}
+              />
+            </label>
+            <p className="privacy-error error-message">{this.props.privacyCheckboxError}</p>
+            <button onClick={this.onSubmit} className={buttonClassName}>
+              {buttonText}
+            </button>
+          </div>
         </div>
       </div>
     );
